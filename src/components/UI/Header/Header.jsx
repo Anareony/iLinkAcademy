eedbed2fe46a9eeef04d51d49e99e2c5dd792ca3
Anareony <chudoyakov.roman@gmail.com>
2022-03-18 09:07:14 +0700

    Added header

--- src/components/UI/Header/Header.jsx
+++ src/components/UI/Header/Header.jsx
@@ -2,23 +2,27 @@ import React from 'react'
 import Button2 from '../Button2/Button2';
 import classes from './Header.module.css'
 import avatar from './img/avatar.svg'
-import ilink from './img/ilink.svg'
-import academy from './img/academy.svg'
+import brand from './img/brand.svg'
 
 const Header = () => {
 
     return (
-        <div className={classes.header}>
-            <div className={classes.user}>
-                <img className={classes.icon} src={avatar} alt='avatar'/>
-                <div className={classes.name}> Роман Чудояков</div>
+        <header className={classes.header}>
+            <div className={classes.container}>
+                <nav className={classes.nav}>
+                    <div className={classes.user}>
+                        <div className={classes.border}>
+                            <img className={classes.icon} src={avatar} alt='avatar'/>
+                        </div>
+                        <div className={classes.name}>Имя Фамилия</div>
+                    </div>
+                    <a href='/' className={classes.link}>
+                        <img src={brand} alt="logo" />
+                    </a>
+                    <Button2>Личный кабинет</Button2>
+                </nav>
             </div>
-            <a href='/'className={classes.link}>
-                <img src={ilink} alt="ilink" />
-                <img src={academy} alt="academy" />
-            </a>
-            <Button2>Личный кабинет</Button2>
-        </div>
+        </header>
     )
 }
 
