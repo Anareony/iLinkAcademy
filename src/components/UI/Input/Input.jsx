0a57ac270dd7c965f454dd5729a05a4588689f7c
Anareony <chudoyakov.roman@gmail.com>
2022-03-22 06:04:14 +0700

    minor fixes

--- src/components/UI/Input/Input.jsx
+++ src/components/UI/Input/Input.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-import classes from './Input.module.css'
+import cl from './Input.module.css'
 
 const Input = (props) => {
   return (
-    <input className={classes.input} {...props}/>
+    <input className={cl.input} {...props}/>
   )
 }
 
