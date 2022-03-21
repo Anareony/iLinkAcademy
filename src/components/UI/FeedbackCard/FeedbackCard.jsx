8c1ee55585dd36706ca028d4375014d1cd8acedd
Anareony <chudoyakov.roman@gmail.com>
2022-03-22 06:03:57 +0700

    add props feedbackcard

--- src/components/UI/FeedbackCard/FeedbackCard.jsx
+++ src/components/UI/FeedbackCard/FeedbackCard.jsx
@@ -1,31 +1,31 @@
 import React from 'react'
-import classes from './FeedbackCard.module.css'
+import cl from './FeedbackCard.module.css'
 import avatar from './img/Camera.svg'
 
 function FeedbackCard(props) {
   return (
-    <div className={classes.feedback}>
-        <div className={classes.container}>
-            <div className={classes.user}>
-                <div className={classes.icon}>
-                    <img src={avatar} className={classes.avatar} alt="" />
+    <div className={cl.feedback}>
+        <div className={cl.container}>
+            <div className={cl.user}>
+                <div className={cl.icon}>
+                    <img src={avatar} className={cl.avatar} alt="" />
                 </div>
-                <div className={classes.about}>
-                    <div className={classes.name}>
-                        Имя Фамилия
+                <div className={cl.about}>
+                    <div className={cl.name}>
+                        {props.reviewer.name}  {props.reviewer.surname}
                     </div>
-                <div className={classes.position}>
-                    Должность (обязательно)
+                <div className={cl.position}>
+                    {props.reviewer.position}
                 </div>
                 </div>
-                <div className={classes.date}>
-                    ДД.ММ.ГГГГ
+                <div className={cl.date}>
+                    {props.reviewer.date}
                 </div>
             </div>
-            <div className={classes.body}>
-                {props.children}
+            <div className={cl.body}>
+                {props.reviewer.body}
             </div>
-            </div>
+        </div>
     </div>
   )
 }
