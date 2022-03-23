b0a8e5d405201bf1f3a6c67ee77113cb34fc0770
Anareony <chudoyakov.roman@gmail.com>
2022-03-24 02:57:37 +0700

    minor fixes and adaptive

--- src/components/UI/Button2/Button2.jsx
+++ src/components/UI/Button2/Button2.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import cl from './Button2.module.css'
+import profile from './img/Profile.svg'
 
 const Button2 = (props) => {
     return (
         <button {...props} className={cl.button}>
-            {props.children}
+            <img className={cl.icon} src={profile} alt='profile'/>
+            <span className={cl.inner}>{props.children}</span>
         </button>
     )
 }
