1ed4fb62496fcdc56489b9cb6260d8e673bb8dc8
Anareony <chudoyakov.roman@gmail.com>
2022-03-24 22:12:51 +0700

    create toast

--- src/components/ToastSucces/ToastSucces.jsx
+++ src/components/ToastSucces/ToastSucces.jsx
@@ -1,25 +1,30 @@
 import React from 'react'
 import cl from './ToastSucces.module.css'
+import cross from './img/cross.svg'
+import success from './img/success.svg'
 
+const ToastSucces = ({show, setShow}) => {
 
-const ToastSucces = ({visibleToast, setVisibleToast}) => {
+	const rootClasses = [cl.toast]
 
-  const rootClasses = [cl.modal]
+	if(show) {
+		rootClasses.push(cl.active)
+	}
 
-  if(visibleToast) {
-      rootClasses.push(cl.active)
-  }
-
-  return (
-    <div className={cl.toast}>
-        <div className="header">
-            Успешно!
-        </div>
-        <div className="content">
-            Спасибо за отзыв о нашей компании :)
-        </div>
-    </div>
-  )
+	return (
+		<div className={rootClasses.join(' ')}>
+			<div className={cl.header}>
+				<p>Успешно!</p>
+				<button onClick={() => setShow(false)} className={cl.button}>
+					<img src={cross} alt='cross'/>
+				</button>
+			</div>
+			<div className={cl.content}>
+				<p>Спасибо за отзыв о нашей<br/> компании :)</p>
+			</div>
+			<img className={cl.success} src={success} alt='succes'/>
+		</div>
+	)
 }
 
 export default ToastSucces
