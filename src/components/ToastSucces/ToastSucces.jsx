09c8a5e1f45eca70d6b93fea4f994cc7fb9dfd6c
Anareony <chudoyakov.roman@gmail.com>
2022-04-14 21:44:53 +0700

    feat: add toastError

--- src/components/ToastSucces/ToastSucces.jsx
+++ src/components/ToastSucces/ToastSucces.jsx
@@ -3,24 +3,28 @@ import cl from './ToastSucces.module.css'
 import cross from './img/cross.svg'
 import success from './img/success.svg'
 
-const ToastSucces = ({show, setShow}) => {
+const ToastSucces = ({show, setShow, title, body}) => {
 
 	const rootClasses = [cl.toast]
 
 	if(show) {
 		rootClasses.push(cl.active)
 	}
+	const close = (e) => {
+        e.preventDefault()
+        setShow(false)
+    }
 
 	return (
 		<div className={rootClasses.join(' ')}>
 			<div className={cl.header}>
-				<p>Успешно!</p>
-				<button onClick={() => setShow(false)} className={cl.button}>
+				<p>{title}</p>
+				<button onClick={close} className={cl.button}>
 					<img src={cross} alt='cross'/>
 				</button>
 			</div>
 			<div className={cl.content}>
-				<p>Спасибо за отзыв о нашей<br/> компании :)</p>
+				<p>{body}</p>
 			</div>
 			<img className={cl.success} src={success} alt='succes'/>
 		</div>
