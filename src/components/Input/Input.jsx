96db1a79bc311642c546f02af9934e20af158574
Anareony <chudoyakov.roman@gmail.com>
2022-04-14 21:46:12 +0700

    refactor(Input): now using styled components

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
-import cl from './Input.module.css'
+import styled from 'styled-components';
+
+const StyledInput = styled.input`
+    font-family: 'Gilroy';
+    font-size: 14px;
+    font-weight: 400;
+    color: #8A8A8A;
+    line-height: 22px;
+    padding: 14px 12px;
+    border: 1px solid #E0E0E0;
+    border-radius: 2px;
+    outline: none;
+    width: 100%;
+
+    &:focus {
+    border: 1px solid #585CC6;
+    }
+`
+
 
 const Input = React.forwardRef((props,ref) => {
   return (
-    <input ref={ref} className={cl.input} {...props}/>
+    <StyledInput ref={ref} {...props}/>
   )
 })
 
