1fd9d50bc6548f91b8a11fb6dc2c121a3560df16
Anareony <chudoyakov.roman@gmail.com>
2022-04-14 21:49:26 +0700

    feat: add authorization and passwordRecovery pages

---
+++ src/pages/Authorization.jsx
@@ -0,0 +1,58 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import styled from 'styled-components';
+
+import Footer from '../components/Footer/Footer'
+import Auth from '../components/Auth/Auth'
+
+import background2 from './img/background2.svg'
+import ilink from './img/ilink.svg'
+
+const Main = styled.main`
+    padding: 40px 406px 452px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    background-color: #585CC6;
+    background-image: url(${background2});
+    background-repeat: no-repeat;
+    background-position: center 150%;
+    background-size: 100%;
+
+    @media (max-width: 1200px) {
+        background-size: 250%;
+        background-position: 50% -50%;
+    }
+    @media (max-width: 900px) {
+        padding: 30px 50px 300px;
+        background-size: 250%;
+        background-position: 50% -50%;
+    }
+    @media (max-width: 430px) {
+        padding: 20px 16px 200px;
+        background-size: 400%;
+        background-position: center -430%;
+    }
+`;
+
+const Wrapper = styled.div`
+
+`
+
+const Authorization = () => {
+
+	return (
+		<Wrapper>
+			<Main>
+                <Link to='/'>
+                    <img src={ilink} alt='ilink'/>
+                </Link>
+				<Auth/>
+			</Main>
+			<Footer/>
+		</Wrapper>
+	)
+}
+
+export default Authorization
