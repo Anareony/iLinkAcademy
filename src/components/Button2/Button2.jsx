bdf70e1aeb58d90d91701fecc4e93ecf1b93ae40
Anareony <chudoyakov.roman@gmail.com>
2022-04-14 21:50:53 +0700

    refactor(Button): now using styled components

--- src/components/Button2/Button2.jsx
+++ src/components/Button2/Button2.jsx
@@ -1,11 +1,37 @@
 import React from 'react'
-import cl from './Button2.module.css'
 
+import styled  from 'styled-components'
+
+const StyledButton = styled.button`
+
+    font-size: 18px;
+    font-family: "Gilroy";
+    font-weight: 600;
+    border: none;
+    border-radius: 2px;
+    padding: 17px 28px;
+    background: #585CC6;
+    line-height: 17px;
+    color: #fff;
+    cursor: pointer;
+
+    &:hover {
+        background: linear-gradient(to right, #696DC8 50%, #585CC6 50%);
+    }
+
+    &:active {
+        background: #797DDF;
+    }
+
+    &:disabled {
+        background: #8A8A8A;
+    }
+ `
 const Button2 = (props) => {
     return (
-        <button {...props} className={cl.button}>
+        <StyledButton {...props}>
             {props.children}
-        </button>
+        </StyledButton>
     )
 }
 
