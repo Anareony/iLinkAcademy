9a15378d6f231aa0c1f85acd81d56eff27223ed2
Anareony <chudoyakov.roman@gmail.com>
2022-04-23 16:55:33 +0700

    feat: add types

---
+++ src/types/types.ts
@@ -0,0 +1,12 @@
+export interface IStudent {
+    id: number;
+    name: string;
+    surname: string;
+    information: string;
+    status: string;
+    about: string;
+    avatar?: string;
+    position?: string;
+    reviewDate: string;
+    reviewStatus: string;
+}
