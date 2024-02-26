bfbb319463f2205bf50fa091b16187813f9610f3
Chudoyakov Roman <chudoyakov.roman@gmail.com>
2024-02-27 00:01:34 +0700

    fix: changed route

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -7,15 +7,12 @@ import PasswordRecovery from '../pages/PasswordRecovery/PasswordRecovery';
 import Students from '../pages/Students/Students';
 import Feedback from '../pages/Feedback/Feedback';
 import AboutMe from '../pages/AboutMe/AboutMe';
-import { getData, getDataReview, setCaptcha } from '../store/store'
 
 const App:React.FC = () => {
-    getData()
-    getDataReview()
     return (
         <Routes>
-            <Route path='/' element={<About/>}/>
-            <Route path='/auth' element={<Authorization/>}/>
+            <Route path='/' element={<Authorization/>}/>
+            <Route path='/about' element={<About/>}/>
             <Route path='/passwordrecovery' element={<PasswordRecovery/>}/>
             <Route path='/students' element={<Students/>}/>
             <Route path='/feedback' element={<Feedback/>}/>
