897749379286856e0ed9d4a5f95436bef3d84374
Chudoyakov Roman <chudoyakov.roman@gmail.com>
2024-02-28 14:47:40 +0700

    feat(userCard): get user profile from store

--- src/pages/About/UserCard/UserCard.tsx
+++ src/pages/About/UserCard/UserCard.tsx
@@ -1,39 +1,44 @@
-import React from 'react'
-
+import React, {useEffect,useState} from 'react'
+import { userStore } from '../../../store/store'
+import { useStore } from 'effector-react'
 import userPhoto from '../../../shared/assets/1.jpg'
 import male from '../assets/Male.svg'
 import dogFood from '../assets/DogFood.svg'
 
 import { UserContainer, UserPhoto, UserInfo, UserName, UserBirthday, UserCity, UserSex, UserAge, UserAbout, UserPets } from './styles'
 
-type IUser = {
-    user: {
-        name: string;
-        surname: string;
-        date: string;
-        city: string;
-        sex: string;
-        age: string;
-        info: string;
-        pets: string;
-    }
-}
 
-const UserCard:React.FC<IUser> = ({user}) => {
-  
+
+const UserCard:React.FC = () => {
+
+    const userInfo = useStore(userStore.$userInfo);
+    const isLoadingUserInfo = useStore(userStore.$isLoading);
+    console.log(userInfo)
+    const user = {
+        sex: 'Мужчина',
+        pets: 'Нет'
+    }
     return (
-        <UserContainer>
-            <UserPhoto src={userPhoto} alt='userPhoto'/>
-            <UserInfo>
-                <UserName><p>{user.name + ' ' + user.surname}</p></UserName>
-                <UserBirthday><p>{user.date}</p></UserBirthday>
-                <UserCity><p><strong>Город:</strong> {user.city}</p></UserCity>
-                <UserSex><p><strong>Пол:</strong> {user.sex}</p><img src={male} alt='sex'/></UserSex>
-                <UserAge><p><strong>Возраст:</strong> {user.age}</p></UserAge>
-                <UserAbout><p><strong>О себе:</strong> {user.info}</p></UserAbout>
-                <UserPets><img src={dogFood} alt='dogFood' style={{marginRight:'12px'}}/><p><strong>Домашнее животное:</strong> {user.pets}</p></UserPets>
-            </UserInfo>
-        </UserContainer>
+        <>
+       {isLoadingUserInfo ?  (
+        <div>   
+            
+        </div>
+        ) : (
+            <UserContainer>
+                {/* <UserPhoto src={`https://academtest.ilink.dev/images/${userInfo.profileImage}`} alt='userPhoto'/> */}
+                <UserInfo>
+                    <UserName><p>{userInfo.firstName} {userInfo.lastName}</p></UserName>
+                    <UserBirthday><p>{new Date(userInfo.birthDate).toLocaleDateString()}</p></UserBirthday>
+                    <UserCity><p><strong>Город:</strong> {userInfo.cityOfResidence}</p></UserCity>
+                    <UserSex><p><strong>Пол:</strong> {user.sex}</p><img src={male} alt='sex'/></UserSex>
+                    <UserAge><p><strong>Возраст:</strong> {userInfo.year}</p></UserAge>
+                    <UserAbout><p><strong>О себе:</strong> test</p></UserAbout>
+                    <UserPets><img src={dogFood} alt='dogFood' style={{marginRight:'12px'}}/><p><strong>Домашнее животное:</strong> {user.pets}</p></UserPets>
+                </UserInfo>
+            </UserContainer>
+        )}
+        </>
     )
 }
 
