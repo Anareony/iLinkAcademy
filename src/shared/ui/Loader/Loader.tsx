78ac08ba5194f85681b9e30e597c4b5032aef1f2
Chudoyakov Roman <chudoyakov.roman@gmail.com>
2024-03-13 18:28:50 +0700

    refactor(fsd): added shared Loader component

---
+++ src/shared/ui/Loader/Loader.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import { White, LightBlue, Blue, Top } from './styles'
+
+export const Loader: React.FC = () => {
+    return (
+        <div>
+            <Top>
+                <White/>
+                <LightBlue/>
+            </Top>
+            <Blue/>
+        </div>
+    )
+}
