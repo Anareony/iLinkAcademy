8c37807f95c27af43526472354b2dfff9b679040
Chudoyakov Roman <chudoyakov.roman@gmail.com>
2024-03-13 18:27:06 +0700

    refactor(fsd): added shared Pagination component

---
+++ src/shared/ui/Pagination/Pagination.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+
+import right from 'shared/assets/right.svg'
+
+import { Img, MyPagination } from './styles'
+
+interface IPaginateProps {
+    marginPagesDisplayed?: number;
+    pagesCount: number;
+    pageRangeDisplayed?: number;
+    onChange: ({ selected }: { selected: number }) => void;
+}
+
+export const Pagination: React.FC<IPaginateProps> = ({
+    marginPagesDisplayed = 2,
+    pagesCount,
+    pageRangeDisplayed = 2,
+    onChange,
+}) => {
+    return (
+        <MyPagination 
+            marginPagesDisplayed={marginPagesDisplayed}
+            pageCount={pagesCount}
+            pageRangeDisplayed={pageRangeDisplayed}
+            onPageChange={onChange}
+            breakLabel="..."
+            previousLabel={
+                <Img src={right} alt='left'/>
+            }
+            nextLabel={
+                <img src={right} alt='right'/>
+            }
+        />
+    )
+}
---
+++ src/shared/ui/Pagination/styles.ts
@@ -0,0 +1,39 @@
+import ReactPaginate from 'react-paginate'
+import styled from 'styled-components'
+
+const Img = styled.img`
+    transform: rotate(180deg);
+`
+
+const MyPagination = styled(ReactPaginate).attrs({
+    activeClassName: 'active'
+})`
+    display: flex;
+    justify-content: center;
+    font-family: 'Gilroy';
+    font-weight: 600;
+    font-size: 14px;
+    line-height: 24px;
+    margin-top: 10px;
+
+    li a {
+        padding: 7px 10px;
+        border-radius: 4px;
+        border: 1px solid #E0E0E0;
+        cursor: pointer;
+        margin-right: 8px;
+        display: inline-block;
+        line-height: 1;
+    }
+
+    li.active a {
+        border: 1px solid #585CC6;
+        color: #585CC6;
+    }
+
+    li.next {
+        display: block;
+    }
+`
+
+export { Img, MyPagination }
