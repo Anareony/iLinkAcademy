a7b326fccb52e0974983c569bac38d5f5cd91184
Chudoyakov Roman <chudoyakov.roman@gmail.com>
2024-03-22 15:58:40 +0700

    refactor: covering in try catch

--- src/shared/api/requests.ts
+++ src/shared/api/requests.ts
@@ -1,108 +1,170 @@
-import { IStatus, IReviewPost, IAuthorization } from "shared/const/types";
+import { IStatus, IReviewPost, IAuthorization, IUpdateUser } from "shared/const/types";
 
 const baseUrl = "https://academtest.ilink.dev";
 
 const authorization = async (loginData: IAuthorization) => {
     const url = `${baseUrl}/user/signIn`
-    const response = await fetch(url, {
-        method: "POST",
-        headers: { 
-            "Content-Type": "application/json" 
-        },
-        body: JSON.stringify(loginData),
-    })
-    .then((response) => response.json())
-    return response.accessToken
+    try {
+        const response = await fetch(url, {
+            method: "POST",
+            headers: { 
+                "Content-Type": "application/json" 
+            },
+            body: JSON.stringify(loginData),
+        })
+        .then((response) => response.json())
+        return response.accessToken
+    } catch (e: any) {
+        throw Error(e.message);
+    }
 }
 
 const getProfile = async () => {
     const url =  `${baseUrl}/user/getUserProfile`
-    const response = await fetch(url, {
-        method: "GET",
-        headers: { 
-            Authorization: `Bearer ${localStorage.getItem("accessToken")}` 
-        },
-    })
-    .then((response) => response.json())
-    return response;
+    try {
+        const response = await fetch(url, {
+            method: "GET",
+            headers: { 
+                Authorization: `Bearer ${localStorage.getItem("accessToken")}` 
+            },
+        })
+        .then((response) => response.json())
+        return response;
+    } catch (e: any) {
+        throw Error(e.message);
+    }
 }
 
 const getCaptcha = async () => {
     const url = `${baseUrl}/reviews/getCaptcha`
-    const response = await fetch(url, {
-        method: "GET",
-    })
-    .then((response) => response.json())
-    return response;
+    try {
+        const response = await fetch(url, {
+            method: "GET",
+        })
+        .then((response) => response.json())
+        return response;
+    } catch (e: any) {
+        throw Error(e.message);
+    }
 }
 
 const getReviews = async () => {
     const url = `${baseUrl}/reviews/getAll`
-    const response = await fetch(url, {
-        method: "GET",
-        headers: { 
-            Authorization: `Bearer ${localStorage.getItem("accessToken")}` 
-        },
-    })
-    .then((response) => response.json())
-    return response;
+    try {
+        const response = await fetch(url, {
+            method: "GET",
+            headers: { 
+                Authorization: `Bearer ${localStorage.getItem("accessToken")}` 
+            },
+        })
+        .then((response) => response.json())
+        return response;
+    } catch (e: any) {
+        throw Error(e.message);
+    }
 }
 
 const getStudents = async () => {
     const url = `${baseUrl}/user/getAll`
-    const response = await fetch(url, {
-        method: "GET",
-        headers: { 
-            Authorization: `Bearer ${localStorage.getItem("accessToken")}` 
-        },
-    })
-    .then((response) => response.json())
-    return response;
+    try {
+        const response = await fetch(url, {
+            method: "GET",
+            headers: { 
+                Authorization: `Bearer ${localStorage.getItem("accessToken")}` 
+            },
+        })
+        .then((response) => response.json())
+        return response;
+    } catch (e: any) {
+        throw Error(e.message);
+    }
 }
 
 const createReview = async (comment: IReviewPost) => {
     const url = `${baseUrl}/reviews/create`
-    const response = await fetch(url, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
-          body: JSON.stringify(comment),
-        }, 
-    })
-    .then((response) => response.json())
-    return response;
+    try {
+        const response = await fetch(url, {
+            method: "POST",
+            headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+            body: JSON.stringify(comment),
+            }, 
+        })
+        .then((response) => response.json())
+        return response;
+    } catch (e: any) {
+        throw Error(e.message);
+    }
 }
 
 const updateStatusComment = async (id: string, status: IStatus) => {
     const url = `${baseUrl}/reviews/updateStatus/${id}`;
-
-    const response = await fetch(url, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${localStorage.getItem("accessToken")!}`,
-        },
-        body: JSON.stringify({ status }),
-    })
-    .then((response) => response.json())
-    return response
+    try {
+        const response = await fetch(url, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${localStorage.getItem("accessToken")!}`,
+            },
+            body: JSON.stringify({ status }),
+        })
+        .then((response) => response.json())
+        return response
+    } catch (e: any) {
+        throw Error(e.message);
+    }
 }
 
 const updateTextComment = async (id: string, text: string) => {
     const url = `${baseUrl}/reviews/updateInfo/${id}`;
-
-    const response = await fetch(url, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${localStorage.getItem("accessToken")!}`,
-        },
-        body: JSON.stringify({ text }),
-    })
-    .then((response) => response.json())
-    console.log('test')
-    return response
+    try {
+        const response = await fetch(url, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${localStorage.getItem("accessToken")!}`,
+            },
+            body: JSON.stringify({ text }),
+        })
+        .then((response) => response.json())
+        return response
+    } catch (e: any) {
+        throw Error(e.message);
+    }
+}
+const updatePhotoUser = async (profileImage: FormData): Promise<any> => {
+    const url = `${baseUrl}/user/updatePhoto`;
+    try {
+        const response = await fetch(url, {
+            method: "POST",
+            headers: {
+                Authorization: `Bearer ${localStorage.getItem("accessToken")!}`,
+            },
+            body: profileImage,
+        })
+        .then((response) => response.json())
+        return response;
+    } catch (e: any) {
+        throw Error(e.message);
+    }
+}
+const updateUser = async (updateInfo: IUpdateUser): Promise<any> => {
+    const url = `${baseUrl}/user/updateInfo`;
+    try {
+        const response = await fetch(url, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${localStorage.getItem("accessToken")!}`,
+            },
+            body: JSON.stringify(updateInfo),
+        })
+        .then((response) => response.json())
+        return response;
+    } catch (e: any) {
+        throw Error(e.message);
+    }
 }
 
 export const API = {
@@ -113,5 +175,7 @@ export const API = {
     createReview,
     getStudents,
     updateStatusComment,
-    updateTextComment
+    updateTextComment,
+    updatePhotoUser,
+    updateUser
 }
