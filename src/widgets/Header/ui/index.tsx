6f7774b05527d9c9771d8d8551e30361227c9c44
Chudoyakov Roman <chudoyakov.roman@gmail.com>
2024-03-29 23:41:06 +0700

    feat: added header widget

---
+++ src/widgets/Header/ui/index.tsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import { useStore } from 'effector-react'
+import { Link } from 'react-router-dom'
+
+import { UserModel } from 'entities/User'
+import { Avatar } from 'shared/ui/Avatar'
+
+import brand from 'shared/assets/brand.svg'
+import profile from 'shared/assets/Profile.svg'
+
+import { Container, Nav, UserInfo, Name, StyledButton, Text, MobileIcon, LastName, Img } from './styles'
+
+export const Header:React.FC = () => {
+    
+    const user = useStore(UserModel.$user)
+
+    return (
+        <header>
+            <Container>
+                <Nav>
+                    <UserInfo>
+                        <Avatar avatar={user.profileImage}/>
+                        <Name>
+                            {user.firstName} 
+                            <LastName>{user.lastName}</LastName>
+                        </Name>
+                    </UserInfo>
+                    <Img src={brand} alt="logo" />
+                    <Link to='/students'>
+                        <StyledButton>
+                            <MobileIcon src={profile} alt='profile'/>
+                            <Text>Панель управления</Text>
+                        </StyledButton>
+                    </Link>
+                </Nav>
+            </Container>
+        </header>
+    )
+}
