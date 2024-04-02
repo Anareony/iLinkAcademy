bc45dbc9bae4429511e78296f258b13e8264ed04
Chudoyakov Roman <chudoyakov.roman@gmail.com>
2024-04-02 22:58:54 +0700

    feat: add prettier

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+	arrowParens: "always",
+	bracketSameLine: false,
+	bracketSpacing: true,
+	singleQuote: false,
+	trailingComma: "all",
+	tabWidth: 4,
+	useTabs: true,
+	semi: true,
+};
